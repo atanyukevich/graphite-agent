b288862c8fa9e9127b7f278b90c5c1412dde0faf
Alexander Tanyukevich <atanyukevich@gmail.com>
2015-09-17 18:38:01 +0200

    because it was counting number of words not lines, so real value is ~%12

--- collectors/tcp_connections.py
+++ collectors/tcp_connections.py
@@ -8,7 +8,7 @@ interval = 60
 class tcp_connections(graphitestat.graphitestat):
 	def get_stat(self):
 		_tcp_file = open('/proc/net/tcp')
-		tcp_connections = len(_tcp_file.read().split())
+		tcp_connections = len(_tcp_file.read().split('\n'))
 		_tcp_file.close()
 		return "net.connections.tcp %d" % tcp_connections
 #-----------------------------------------------------------------------------#
